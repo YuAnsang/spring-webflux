f173be394d7fd596122e442ad67ed2e900b6ee5b
YuAnsang <kahlman@naver.com>
2023-08-06 23:51:10 +0900

    WebFlux Example v2

---
+++ src/main/java/com/asyu/github/springwebflux/webflux/v2/BookController.java
@@ -0,0 +1,37 @@
+package com.asyu.github.springwebflux.webflux.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RequestMapping("/v2/books")
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    private final BookMapper mapper = BookMapper.INSTANCE;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<BookDto.Response> postBook(@RequestBody Mono<BookDto.Post> requestBody) {
+        Mono<Book> result = bookService.createBook(requestBody);
+        return result.flatMap(book -> Mono.just(mapper.bookToResponse(book)));
+    }
+
+    @PatchMapping("/{book-id}")
+    public Mono<BookDto.Response> patchBook(@PathVariable("book-id") long bookId,
+                          @RequestBody Mono<BookDto.Patch> requestBody) {
+        Mono<Book> result = bookService.updateBook(bookId, requestBody);
+        return result.flatMap(book -> Mono.just(mapper.bookToResponse(book)));
+    }
+
+    @GetMapping("/{book-id}")
+    public Mono<BookDto.Response> getBook(@PathVariable("book-id") long bookId) {
+        return bookService.findBook(bookId)
+                .flatMap(book -> Mono.just(mapper.bookToResponse(book)));
+    }
+
+}
